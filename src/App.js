f4f726a7d1c84eb77ce27afd25894d17583af334
Dan Lubbers <lubbers.dan@gmail.com>
2019-10-08 14:49:21 -0400

    Finished Tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const INITIAL_GAME_STATE = {victory: false, startTime: null, endTime: null };
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const [snippet, setSnippet] = useState('');
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log({snippetIndex});
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({...gameState, startTime: new Date().getTime() });
+  }
+
+  // useState('') is setting the default value of an empty string to the variables 'userText and 'setUserText'
+  const [userText, setUserText] = useState('');
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log({userText});
+
+    if(event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true, 
+        endTime: new Date().getTime() - gameState.startTime
+       })
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Type Race</h2>
+    <hr />
+    <h3>Snippet</h3>
+    {snippet}
+    <h4>{gameState.victory ? `Done! Time: ${gameState.endTime}ms` : null}</h4>
+      <input value={userText} onChange={updateUserText}/>
+      <hr/>
+      {
+        SNIPPETS.map((SNIPPET, index) => (
+          <button onClick={chooseSnippet(index)} key={index}>
+            {SNIPPET.substring(0, 10)}...
+          </button>
+        ))
+      }
     </div>
   );
 }
